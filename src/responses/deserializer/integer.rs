77af2ac6f7de314834afbddd7f4d570b578c35e4
Jichao Sun <4977515+JichaoS@users.noreply.github.com>
2023-05-09 01:10:37 -0700

    Fixes to integer type deserialization
     - Encodes the resulting value type according to RowType.value_type()
     - Let value_type define how to decode i64/i128 for JSON. (centralizes the precision logic)
     - Reduce precision upper bound for i64 to 18. (i64::MAX is 19 digits)

--- src/responses/deserializer/integer.rs
+++ src/responses/deserializer/integer.rs
@@ -1,59 +1,63 @@
 use crate::errors::SnowflakeError;
 use crate::responses::types::{row_type::RowType, value::Value};
+use decimal_rs::Decimal;
 
 use serde_json;
 
-pub(super) fn from_json(json: &str, row_type: &RowType) -> Result<Value, SnowflakeError> {
-    let value = if row_type.precision <= Some(19) {
-        let parsed: i64 = serde_json::from_str(json).map_err(|e| {
-            SnowflakeError::new_deserialization_error_with_field_and_value(
-                e.into(),
-                row_type.name.clone(),
-                json.to_string(),
-            )
-        })?;
-
-        Value::I64(parsed)
-    }
-    else {
-        let parsed: i128 = serde_json::from_str(json).map_err(|e| {
-            SnowflakeError::new_deserialization_error_with_field_and_value(
-                e.into(),
-                row_type.name.clone(),
-                json.to_string(),
-            )
-        })?;
-
-        Value::I128(parsed)
-    };
-
-    if row_type.nullable {
+fn wrap_in_nullable(value: Value, is_nullale: bool) -> Value {
+    if is_nullale {
         let boxed = Box::new(value);
-        Ok(Value::Nullable(Some(boxed)))
+        Value::Nullable(Some(boxed))
     }
     else {
-        Ok(value)
+        value
     }
+}
+
+pub(super) fn i64_from_json(json: &str, row_type: &RowType) -> Result<Value, SnowflakeError> {
+    let parsed: i64 = serde_json::from_str(json).map_err(|e| {
+        SnowflakeError::new_deserialization_error_with_field_and_value(
+            e.into(),
+            row_type.name.clone(),
+            json.to_string(),
+        )
+    })?;
+    let value = Value::I64(parsed);
+
+    Ok(wrap_in_nullable(value, row_type.nullable))
+}
+
+pub(super) fn i128_from_json(json: &str, row_type: &RowType) -> Result<Value, SnowflakeError> {
+    let parsed: i128 = serde_json::from_str(json).map_err(|e| {
+        SnowflakeError::new_deserialization_error_with_field_and_value(
+            e.into(),
+            row_type.name.clone(),
+            json.to_string(),
+        )
+    })?;
+    let value = Value::I128(parsed);
+
+    Ok(wrap_in_nullable(value, row_type.nullable))
 }
 
 #[cfg(feature = "arrow")]
 pub(super) fn from_arrow(
     column: &dyn arrow2::array::Array,
     field: &arrow2::datatypes::Field,
+    value_type: &crate::responses::types::value::ValueType,
 ) -> Result<Vec<Value>, SnowflakeError> {
-    use crate::responses::deserializer::decimal::from_arrow as decimal_from_arrow;
+    use crate::responses::{deserializer::decimal::from_arrow as decimal_from_arrow};
     use anyhow::anyhow;
     use arrow2::datatypes::DataType;
-
     match field.data_type {
-        DataType::Int8 => downcast_integer::<i8>(column, field),
-        DataType::UInt8 => downcast_integer::<u8>(column, field),
-        DataType::Int16 => downcast_integer::<i16>(column, field),
-        DataType::UInt16 => downcast_integer::<u16>(column, field),
-        DataType::Int32 => downcast_integer::<i32>(column, field),
-        DataType::UInt32 => downcast_integer::<u32>(column, field),
-        DataType::Int64 => downcast_integer::<i64>(column, field),
-        DataType::UInt64 => downcast_integer::<u64>(column, field),
+        DataType::Int8 => downcast_integer::<i8>(column, field, value_type),
+        DataType::UInt8 => downcast_integer::<u8>(column, field, value_type),
+        DataType::Int16 => downcast_integer::<i16>(column, field, value_type),
+        DataType::UInt16 => downcast_integer::<u16>(column, field, value_type),
+        DataType::Int32 => downcast_integer::<i32>(column, field, value_type),
+        DataType::UInt32 => downcast_integer::<u32>(column, field, value_type),
+        DataType::Int64 => downcast_integer::<i64>(column, field, value_type),
+        DataType::UInt64 => downcast_integer::<u64>(column, field, value_type),
         DataType::Decimal(_, scale) => decimal_from_arrow(&scale, column, field),
         _ => Err(SnowflakeError::new_deserialization_error_with_field(
             anyhow!("Invalid integer data type {:?}", field.data_type),
@@ -66,6 +70,7 @@ pub(super) fn from_arrow(
 fn downcast_integer<T: arrow2::types::NativeType + num::NumCast>(
     column: &dyn arrow2::array::Array,
     field: &arrow2::datatypes::Field,
+    value_type: &crate::responses::types::value::ValueType,
 ) -> Result<Vec<Value>, SnowflakeError> {
     use crate::responses::deserializer::null::from_arrow as null_from_arrow;
     use crate::utils::until_err;
@@ -84,13 +89,20 @@ fn downcast_integer<T: arrow2::types::NativeType + num::NumCast>(
                         None => return null_from_arrow(field),
                     };
 
-                    if field.is_nullable {
-                        let boxed = Box::new(Value::I64(value));
-                        Ok(Value::Nullable(Some(boxed)))
-                    }
-                    else {
-                        Ok(Value::I64(value))
-                    }
+                    // The arrow return types sometimes do not match with value_type.
+                    // Ex: SNOWFLAKE.ACCOUNT_USAGE.QUERY_HISTORY.total_elapsed_time is a Decimal(28,0) in value_type but I64 in arrow2.
+                    // Here we make sure to encode the result as the expected value_type.
+                    let wrapped_value = match value_type {
+                        crate::responses::types::value::ValueType::Decimal => Value::Decimal(Decimal::from(value)),
+                        crate::responses::types::value::ValueType::I128 => Value::I128(value.into()),
+                        crate::responses::types::value::ValueType::I64 => Value::I64(value),
+                        _ => Err(SnowflakeError::new_deserialization_error_with_field(
+                            anyhow!("Invalid integer value type {:?}", value_type),
+                            field.name.clone(),
+                        ))?,
+                    };
+                    
+                    Ok(wrap_in_nullable(wrapped_value, field.is_nullable))
                 })
                 .scan(&mut err, until_err)
                 .collect();
@@ -102,7 +114,7 @@ fn downcast_integer<T: arrow2::types::NativeType + num::NumCast>(
         }
         None => Err(SnowflakeError::new_deserialization_error_with_field(
             anyhow!(
-                "Could not convert primitive array of type {:?} to i128",
+                "Could not convert primitive array of type {:?} to i64",
                 field.data_type
             ),
             field.name.clone(),
